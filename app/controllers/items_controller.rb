9c86623c1396e3761dbe0a4427cf097206985b7f
Isaiah <ijandicos90@gmail.com>
2022-08-25 04:18:35 -0400

    Completed Assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,42 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = find_item
+    render json: item
+  end
+
+  def create
+    user = find_user
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def find_item
+    Item.find(params[:id])
+  end
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
   end
 
 end
